28fd04ea19a9234418912c931f13ef42c126cf9d
vinnyA3 <vincent.aceto@gmail.com>
2026-07-29 21:57:45 -0400

    feat(modules): networking - add rn bundle port

--- modules/networking.nix
+++ modules/networking.nix
@@ -2,6 +2,6 @@
   # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 53317 ]; # localsend
+  networking.firewall.allowedTCPPorts = [ 53317 8081 ]; # localsend - 53317; 8081 react-native
   networking.firewall.allowedUDPPorts = [ 53317 ];
 }
