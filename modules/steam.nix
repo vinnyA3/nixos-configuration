c0831c837a2b6b56d80a349a80d86e635c775128
vinnyA3 <vincent.aceto@gmail.com>
2026-08-04 19:25:12 -0400

    feat(steam): add nexus mods
    Permitting nexus mods - was built from source (need to see why cache was
    missed)

--- modules/steam.nix
+++ modules/steam.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 {
+  nixpkgs.config.permittedInsecurePackages = [
+    "nexusmods-app-unfree-0.21.1"
+  ];
+
   programs.steam = {
     enable = true;
     package = pkgs.millennium-steam;
